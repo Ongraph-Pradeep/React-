8b1defa4e6e6ca246ad77843825409369c50889f
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-06 17:17:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Pradeep pushing code from from Jenkins Pipeline.
+          This is Gosala Pradeep pushing code from from Jenkins Pipeline.
         </p>
         <a
           className="App-link"
