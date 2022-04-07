0f5f9174ec61e78890ba9c2ec9c365d8e8ac9ed9
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-07 12:38:30 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Gosala Pradeep pushing code from Github to React Application using Jenkins Pipeline.
+          This is Gosala Pradeep pushing code from Github to React Application.
         </p>
         <a
           className="App-link"
