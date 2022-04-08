52105b3f2c9d6d8adb282d00348a848261e071f2
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-08 10:28:24 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Gosala Pradeep .
+          This is Gosala Pradeep pushing the code from Github to React Application .
         </p>
         <a
           className="App-link"
