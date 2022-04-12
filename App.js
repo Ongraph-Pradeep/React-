6091cc7098c6f53c0d2665dcd7841770bd4cf592
gosalapradeep <79558129+gosalapradeep@users.noreply.github.com>
2022-04-12 10:47:23 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is Gosala Pradeep 
+          This is Gosala Pradeep pushing the code from the repo.
             .
         </p>
         <a
